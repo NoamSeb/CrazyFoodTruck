048270965c6a3018f3cb80c9192d346a1095f300
Scorp <margotdelage02@gmail.com>
2025-10-16 09:31:58 +0200

    <Add> BP_HordeManager : ajout de horde manager
    spawn un nombre de zombies aléatoire à des positions aléatoire dans le rectangle du BP

---
+++ Source/CrazyFoodTruck/HordeManager.cpp
@@ -0,0 +1,75 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HordeManager.h"
+#include "GameFramework/Character.h"
+
+
+// Sets default values
+AHordeManager::AHordeManager()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SpawnZone = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnZone"));
+	RootComponent = SpawnZone;
+
+	// Couleur / visibilité dans l'éditeur
+	SpawnZone->SetBoxExtent(FVector(500.0f, 500.0f, 200.0f)); // taille par défaut
+	SpawnZone->SetHiddenInGame(true);
+	SpawnZone->SetVisibility(true);
+	SpawnZone->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AHordeManager::SpawnHordeZombie()
+{
+	FVector BoxExtent = SpawnZone->GetScaledBoxExtent();
+
+	srand(time(0));
+	int8 numberZombies = rand() % 10 + 5;
+
+	GEngine->AddOnScreenDebugMessage(1, 10.0f, FColor::Red, FString::Printf(TEXT("Number Zombies: %d"), numberZombies));
+	
+	for (int i = 0; i < numberZombies; i++)
+	{
+		//FVector SpawnLocation(FMath::RandRange(-BoxExtent.X, BoxExtent.X), FMath::RandRange(-BoxExtent.Y, BoxExtent.Y), 90.0f);
+		FVector SpawnLocation = SpawnZone->GetComponentLocation() + FVector(
+	FMath::RandRange(-BoxExtent.X, BoxExtent.X),
+	FMath::RandRange(-BoxExtent.Y, BoxExtent.Y),
+	90.0f
+);
+		FRotator SpawnRotation(0.0f, 0.0f, 0.0f);
+		FVector SpawnScale(1.0f, 1.0f, 1.0f);
+	
+		
+		FTransform NewTransform;
+		NewTransform.SetLocation(SpawnLocation);
+		NewTransform.SetScale3D(SpawnScale);
+
+		ACharacter* NewZombie = GetWorld()->SpawnActor<ACharacter>(PawnZombie, NewTransform);
+		ListHordeZombie.Add(NewZombie);
+
+		NewZombie->SpawnDefaultController();
+
+		//à voir pour remplacer plus tard le ce round d'ici pas un custom event init
+		UFunction* EventRoundZombie = NewZombie->FindFunction(FName("CE_Round"));
+		if (EventRoundZombie)
+		{
+			NewZombie->ProcessEvent(EventRoundZombie, nullptr);
+		}
+	}
+}
+
+// Called when the game starts or when spawned
+void AHordeManager::BeginPlay()
+{
+	Super::BeginPlay();
+	SpawnHordeZombie();
+}
+
+// Called every frame
+void AHordeManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/CrazyFoodTruck/HordeManager.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "stdlib.h"
+#include "time.h"
+#include "Components/BoxComponent.h"
+#include "HordeManager.generated.h"
+
+UCLASS()
+class CRAZYFOODTRUCK_API AHordeManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AHordeManager();
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<ACharacter> PawnZombie = AActor::StaticClass();
+	void SpawnHordeZombie();
+	UPROPERTY(VisibleAnywhere)
+	TArray<ACharacter*> ListHordeZombie;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Spawn Area")
+	UBoxComponent* SpawnZone;
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
