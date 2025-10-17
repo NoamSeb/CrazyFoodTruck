0368691aff5c9e0d5e68ba59a028ca4dbabdc62b
Scorp <margotdelage02@gmail.com>
2025-10-17 11:36:31 +0200

    <Add> base settings : create game instance, game mode and HUD

---
+++ Source/CrazyFoodTruck/GameModeCrazyFoodTruck.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GameModeCrazyFoodTruck.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRAZYFOODTRUCK_API AGameModeCrazyFoodTruck : public AGameMode
+{
+	GENERATED_BODY()
+};
