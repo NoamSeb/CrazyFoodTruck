41a24ae9e7c7b6abdaf60f86eeb08f507a821c80
perkaBrioche <lemergabriel55@gmail.com>
2025-10-23 11:52:14 +0200

    <Add><Setup Interaction Widget>

---
+++ Source/CrazyFoodTruck/Public/Widget/UInteractBoxWidget.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UInteractBoxWidget.generated.h"
+
+UCLASS()
+class CRAZYFOODTRUCK_API UUInteractBoxWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public :
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void ShowInteract();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void HideInteract();
+	
+};
