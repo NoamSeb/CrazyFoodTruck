1c8a226c534d22e97a14dd469f5bb0e51538a4c0
maximedesnoues <maxime.desnoues@orange.fr>
2025-11-18 14:00:14 +0100

    Add: Score display in widget + Player movement bug fixed

--- Source/CrazyFoodTruck/Private/Characters/CrazyFoodTruckCharacter.cpp
+++ Source/CrazyFoodTruck/Private/Characters/CrazyFoodTruckCharacter.cpp
@@ -14,11 +14,17 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 
-// Sets default values
+static void BasisFromYaw(const float YawDeg, FVector& OutForward, FVector& OutRight)
+{
+    const FRotator Flat(0.f, YawDeg, 0.f);
+    const FRotationMatrix RM(Flat);
+    OutForward = RM.GetUnitAxis(EAxis::X);
+    OutRight = RM.GetUnitAxis(EAxis::Y);
+}
+
 ACrazyFoodTruckCharacter::ACrazyFoodTruckCharacter()
 {
- 	// Set this character to call Tick() every frame. You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
     if (UCharacterMovementComponent* Move = GetCharacterMovement())
     {
@@ -28,7 +34,6 @@ ACrazyFoodTruckCharacter::ACrazyFoodTruckCharacter()
     }
 }
 
-// Called when the game starts or when spawned
 void ACrazyFoodTruckCharacter::BeginPlay()
 {
 	Super::BeginPlay();
@@ -41,13 +46,6 @@ void ACrazyFoodTruckCharacter::BeginPlay()
     UpdatePlayerColorFromController();
 }
 
-// Called every frame
-void ACrazyFoodTruckCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
-// Called to bind functionality to input
 void ACrazyFoodTruckCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
@@ -59,45 +57,21 @@ void ACrazyFoodTruckCharacter::SetupPlayerInputComponent(UInputComponent* Player
     }
 }
 
-int32 ACrazyFoodTruckCharacter::GetPlayerIndex() const
+void ACrazyFoodTruckCharacter::SetVehicleMovementRef(AActor* InVehicleActor)
 {
-    const APlayerController* PC = Cast<APlayerController>(Controller);
-    if (!PC) return -1;
-
-    const ULocalPlayer* LP = PC->GetLocalPlayer();
-    return LP ? LP->GetControllerId() : -1;
+    VehicleRefActor = InVehicleActor;
 }
 
-FLinearColor ACrazyFoodTruckCharacter::GetPlayerColor() const
+void ACrazyFoodTruckCharacter::UseWorldFrame()
 {
-    return PlayerColor;
+    MovementFrame = EMovementFrame::World;
+    VehicleRefActor = nullptr;
 }
 
-void ACrazyFoodTruckCharacter::SetPlayerColor(FLinearColor NewColor)
+void ACrazyFoodTruckCharacter::UseVehicleFrame(AActor* InVehicle)
 {
-    PlayerColor = NewColor;
-}
-
-void ACrazyFoodTruckCharacter::UpdatePlayerColorFromController()
-{
-    switch (GetPlayerIndex())
-    {
-    case 0:
-        PlayerColor = FLinearColor(0.0f, 0.45f, 1.0f);
-        break;
-    case 1:
-        PlayerColor = FLinearColor(0.1f, 0.8f, 0.2f); 
-        break;
-    case 2:
-        PlayerColor = FLinearColor(1.0f, 0.1f, 0.1f); 
-        break;
-    case 3:
-        PlayerColor = FLinearColor(1.0f, 0.9f, 0.1f);
-        break;
-    default:
-        PlayerColor = FLinearColor::White;
-        break;
-    }
+    MovementFrame = EMovementFrame::Vehicle;
+    VehicleRefActor = InVehicle;
 }
 
 void ACrazyFoodTruckCharacter::SetInputData(UCrazyFoodTruckCharacterInputData* InInputData)
@@ -130,12 +104,12 @@ void ACrazyFoodTruckCharacter::SetupMappingContextIntoController() const
     }
 }
 
-static void BasisFromYaw(const float YawDeg, FVector& OutForward, FVector& OutRight)
+void ACrazyFoodTruckCharacter::BindInputMoveAction(UEnhancedInputComponent* EnhancedInputComponent)
 {
-    const FRotator Flat(0.f, YawDeg, 0.f);
-    const FRotationMatrix RM(Flat);
-    OutForward = RM.GetUnitAxis(EAxis::X);
-    OutRight = RM.GetUnitAxis(EAxis::Y);
+    if (InputData && InputData->InputActionMove)
+    {
+        EnhancedInputComponent->BindAction(InputData->InputActionMove, ETriggerEvent::Triggered, this, &ACrazyFoodTruckCharacter::OnInputMove);
+    }
 }
 
 void ACrazyFoodTruckCharacter::OnInputMove(const FInputActionValue& InputActionValue)
@@ -156,9 +130,17 @@ void ACrazyFoodTruckCharacter::OnInputMove(const FInputActionValue& InputActionV
     {
     case EMovementFrame::Vehicle:
     {
-        if (!VehicleRefActor.IsValid()) return;
-        const float Yaw = VehicleRefActor->GetActorRotation().Yaw + MovementYawOffsetDegrees;
-        BasisFromYaw(Yaw, Forward, Right);
+        if (VehicleRefActor.IsValid())
+        {
+            const float Yaw = VehicleRefActor->GetActorRotation().Yaw + MovementYawOffsetDegrees;
+            BasisFromYaw(Yaw, Forward, Right);
+        }
+        else
+        {
+            const APlayerController* PC = Cast<APlayerController>(Controller);
+            const float Yaw = (PC ? PC->GetControlRotation().Yaw : GetActorRotation().Yaw) + MovementYawOffsetDegrees;
+            BasisFromYaw(Yaw, Forward, Right);
+        }
         break;
     }
     case EMovementFrame::World:
@@ -173,14 +155,6 @@ void ACrazyFoodTruckCharacter::OnInputMove(const FInputActionValue& InputActionV
 
     AddMovementInput(Forward, Y);
     AddMovementInput(Right, X);
-}
-
-void ACrazyFoodTruckCharacter::BindInputMoveAction(UEnhancedInputComponent* EnhancedInputComponent)
-{
-    if (InputData && InputData->InputActionMove)
-    {
-        EnhancedInputComponent->BindAction(InputData->InputActionMove, ETriggerEvent::Triggered, this, &ACrazyFoodTruckCharacter::OnInputMove);
-    }
 }
 
 void ACrazyFoodTruckCharacter::BindInputInteractAction(UEnhancedInputComponent* EnhancedInputComponent)
@@ -239,23 +213,6 @@ AAmmoBox* ACrazyFoodTruckCharacter::DepositAmmoBox()
     return CarriedAmmoBox;
 }
 
-void ACrazyFoodTruckCharacter::SetVehicleMovementRef(AActor* InVehicleActor)
-{
-    VehicleRefActor = InVehicleActor;
-}
-
-void ACrazyFoodTruckCharacter::UseWorldFrame()
-{
-    MovementFrame = EMovementFrame::World;
-    VehicleRefActor = nullptr;
-}
-
-void ACrazyFoodTruckCharacter::UseVehicleFrame(AActor* InVehicle)
-{
-    MovementFrame = EMovementFrame::Vehicle;
-    VehicleRefActor = InVehicle;
-}
-
 const TScriptInterface<IInteractable>& ACrazyFoodTruckCharacter::GetFocusedInteractable() const
 {
     return FocusedInteractable;
@@ -264,4 +221,45 @@ const TScriptInterface<IInteractable>& ACrazyFoodTruckCharacter::GetFocusedInter
 void ACrazyFoodTruckCharacter::SetFocusedInteractable(const TScriptInterface<IInteractable>& NewTarget)
 {
     FocusedInteractable = NewTarget;
+}
+
+int32 ACrazyFoodTruckCharacter::GetPlayerIndex() const
+{
+    const APlayerController* PC = Cast<APlayerController>(Controller);
+    if (!PC) return -1;
+
+    const ULocalPlayer* LP = PC->GetLocalPlayer();
+    return LP ? LP->GetControllerId() : -1;
+}
+
+FLinearColor ACrazyFoodTruckCharacter::GetPlayerColor() const
+{
+    return PlayerColor;
+}
+
+void ACrazyFoodTruckCharacter::SetPlayerColor(FLinearColor NewColor)
+{
+    PlayerColor = NewColor;
+}
+
+void ACrazyFoodTruckCharacter::UpdatePlayerColorFromController()
+{
+    switch (GetPlayerIndex())
+    {
+    case 0:
+        PlayerColor = FLinearColor(0.0f, 0.45f, 1.0f);
+        break;
+    case 1:
+        PlayerColor = FLinearColor(0.1f, 0.8f, 0.2f); 
+        break;
+    case 2:
+        PlayerColor = FLinearColor(1.0f, 0.1f, 0.1f); 
+        break;
+    case 3:
+        PlayerColor = FLinearColor(1.0f, 0.9f, 0.1f);
+        break;
+    default:
+        PlayerColor = FLinearColor::White;
+        break;
+    }
 }
