91bd94326fb3a9dd5a98152d690670f874612fb0
william_reppelin.github.io <william.reppelin@edu.devinci.fr>
2025-12-08 11:15:56 +0100

    test will

---
+++ Source/CrazyFoodTruck/Public/TargetManager.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TargetManager.generated.h"
+
+UCLASS()
+class CRAZYFOODTRUCK_API ATargetManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATargetManager();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	UPROPERTY(EditAnywhere, Category="Target Parameters")
+	APawn* TargetPawn;
+	
+	virtual void Tick(float DeltaTime) override;
+};
